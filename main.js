dfbbe69329161a8ec664494c0434830420b5694e
“Moggach” <acunnane13@gmail.com>
2021-08-19 19:52:25 +0100

    added function to output n number of primes onto page

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
-//test to see if a number is prime
-
+//function testing to see if number is prime using seive of
 function isPrime(num) {
-  for (let i = 2; i < num; i++) {
+  const numberSqrt = Math.floor(Math.sqrt(num));
+  let i = 2;
+  while (i <= numberSqrt) {
     if (num % i === 0) {
       return false;
     }
+
+    i += 1;
   }
+
   return true;
 }
 
-//function logs out prime values
-function primesArray(n) {
+//creates array of prime values
+function primesArray(num) {
   //initialising array
   let primes = [];
   //avoid returning 1 as a prime value
   let i = 2;
 
   //setting condition - total number of values returned should be equal to n
-  while (primes.length < n) {
+  while (primes.length < num) {
     //if i passes prime test add it to the array
     if (isPrime(i)) {
       primes.push(i);
@@ -28,37 +32,17 @@ function primesArray(n) {
   return primes;
 }
 
-//function testing to see if number is prime using seive of
-function isPrime(num) {
-  const squareRoot = Math.sqrt(num);
-
-  for (let i = 2; i < squareRoot; i += 1) {
-    if (num % i === 0) {
-      return false;
-    }
-  }
-  return true;
-}
-
-//testing primesArray function
-console.log(primesArray(100));
-console.log(primesArray(1000));
-
-//building template string for multiplication table
-function outputPrimes() {
-  const number = document.getElementById('number').value;
-  const output = `
-<div class="output-primes">
-<p>${number}</p>
-</div>`;
-  const display = document.getElementById('output-primes');
-  display.innerHTML = output;
-}
-
 //funtion to handle form submit
 function onSubmit(e) {
   e.preventDefault();
-  outputPrimes();
+  const num = document.getElementById('number').value;
+  const data = primesArray(num);
+  const output = `
+  <div class="output-primes">
+  <p>${data}</p>
+  </div>`;
+  const display = document.getElementById('output-primes');
+  display.innerHTML = output;
 }
 
 //adding event listener to submit button
