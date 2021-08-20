32f0ab5ca87e58815755ac164694f9690a8a1185
“Moggach” <acunnane13@gmail.com>
2021-08-20 14:31:26 +0100

    adding function to generate nested arrays with multiplication table

--- main.js
+++ main.js
@@ -1,42 +1,12 @@
-//function testing to see if number is prime using seive of
-function isPrime(num) {
-  const numberSqrt = Math.floor(Math.sqrt(num));
-  let i = 2;
-  while (i <= numberSqrt) {
-    if (num % i === 0) {
-      return false;
-    }
+//declaring global variable input
+var input;
 
-    i += 1;
-  }
-
-  return true;
-}
-
-//creates array of prime values
-function primesArray(num) {
-  //initialising array
-  let primes = [];
-  //avoid returning 1 as a prime value
-  let i = 2;
-
-  //setting condition - total number of values returned should be equal to n
-  while (primes.length < num) {
-    //if i passes prime test add it to the array
-    if (isPrime(i)) {
-      primes.push(i);
-    }
-    //increment in and add 1 to i
-    i += 1;
-  }
-  return primes;
-}
-
-//funtion to handle form submit
+//funtion to handle form submit and update input value
 function onSubmit(e) {
   e.preventDefault();
-  const num = document.getElementById('number').value;
-  const data = primesArray(num);
+  input = document.getElementById('number').value;
+  const data = multiplyPrimes(input);
+  console.log(data);
   const output = `
   <div class="output-primes">
   <p>${data}</p>
@@ -47,3 +17,57 @@ function onSubmit(e) {
 
 //adding event listener to submit button
 document.getElementById('submit').addEventListener('click', onSubmit);
+
+//function testing to see if number is prime using seive of Eratosthenes
+function isPrime(input) {
+  const numberSqrt = Math.floor(Math.sqrt(input));
+  let i = 2;
+  while (i <= numberSqrt) {
+    if (input % i === 0) {
+      return false;
+    }
+
+    i += 1;
+  }
+
+  return true;
+}
+
+//creates array of prime values
+function primesArray(input) {
+  //initialising array
+  let primes = [];
+  //avoid returning 1 as a prime value
+  let i = 2;
+
+  //setting condition - total number of values returned should be equal to n
+  while (primes.length < input) {
+    //if i passes prime test add it to the array
+    if (isPrime(i)) {
+      primes.push(i);
+    }
+    //increment in and add 1 to i
+    i += 1;
+  }
+  return primes;
+}
+
+//function to generate nested array for multiplication table
+function multiplyPrimes(input) {
+  let primes = primesArray(input);
+  let initArr = ['|'].concat(primes);
+
+  let product = [initArr];
+
+  primes.forEach(function (prime) {
+    let nextRow = [prime];
+
+    primes.forEach(function (coefficient) {
+      nextRow.push(prime * coefficient);
+    });
+
+    product.push(nextRow);
+  });
+
+  return product;
+}
